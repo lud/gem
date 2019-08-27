4c9fea1c5a2b9e39eaba1682f6dcdc3e6896cd29
lud <ludovic@demblans.com>
2019-08-27 12:14:09 +0200

    added lint command

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Gem.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -23,6 +24,12 @@ defmodule Gem.MixProject do
       {:cubdb, "~> 0.12.0", only: [:test, :dev]},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:test, :dev], runtime: false},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: ["dialyzer", "credo --all --strict"]
     ]
   end
 end
